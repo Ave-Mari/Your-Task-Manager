dc78597e0f5811278c541cf8b5083f04141964f8
Mari Vaypan <avemari98@gmail.com>
2023-06-28 02:33:18 +0300

    add text to hpmepage and styles

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>Homepage</h1>
+    <section class="homepage">
+        <h1>Homepage</h1>
+        <p>Welcome to your personal task manager where you can <router-link class="homepage-link" to="/add-task">create</router-link> task and add it to <router-link class="homepage-link" to="/tasks-list">list</router-link></p>
+      </section>
+   
 </template>
 
 <script>
@@ -9,5 +13,19 @@ export default {
         return {}
     }
 }
-
 </script>
+
+<style>
+
+    .homepage-link {
+        
+    }
+
+    .homepage {
+        background: #fff;
+    }
+
+    p {
+        font-weight: 300;
+    }
+</style>
